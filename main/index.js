a8a114c4b3d69ad4c5da94d05eef27d9bed8240f
will198735 <recinoswilfredo@gmail.com>
2023-02-19 13:34:34 -0800

    modified index.js and template file

--- main/index.js
+++ main/index.js
@@ -1,11 +1,14 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
 // const Employee = require('./lib/Employee');
 const Engineer = require('./lib/Engeneer');
 const Intern = require('./lib/Intern');
 const Manager = require('./lib/Manager');
-const generateHTTML = require('./src/generatehtml.js');
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+const inquirer = require('inquirer');
+const path = require('path');
+const fs = require('fs');
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(outputPath, "team.html");
+const generateTeam = require('./src/template.js');
+
 
 
 teamArray = [];
@@ -147,12 +150,13 @@ function addIntern() {
 function htmlBuilder () {
   console.log("Team created!")
 
-  fs.writeFileSync(outputPath, generateHTTML(teamArray), "UTF-8")
+  fs.writeFileSync(outputPath, generateTeam(teamArray), "UTF-8")
 
 }
-}
+
 
 createTeam();
+}
 
 
 
--- main/src/template.js
+++ main/src/template.js
@@ -1,45 +1,13 @@
 
 
 
-    module.exports = manager => {
-        return
-    `<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-    <title>Document</title>
-  </head>
-  <body>
-  <div class="container-fluid">
-        <div class="row">
-            <div class="col-12 jumbotron mb-3 team-heading bg-danger">
-                <h1 class="text-center text-white">My Team</h1>
-            </div>
-        </div>
-    </div>
-    <div class="container">
-        <div class="row">
-            <div class="row team-area col-12 d-flex justify-content-center">
-                ${generateProfile(profile)}
-            </div>
-        </div>
-    </div>
-  
-
-  </body>
-  </html>
-  `;
-    }
+   
+const generateProfile = team => {
 
 
-const generateProfile = profile => {
-
-
-  const htmlManager = manager => {
-    return
-   `  <header class="p-5 mb-4 header bg-light">
+  const generateManager = manager => {
+   
+  return `  <header class="p-5 mb-4 header bg-light">
     <h2 class="card-title">${manager.getName()}</h2>
     <h2 class="card-title">${manager.getRole()}</h2>
     </header>
@@ -58,12 +26,12 @@ const generateProfile = profile => {
   </div>
   `;
 };
-const htmlEngeneer = engeneer  => { 
-    return
+const generateEngineer = engineer  => { 
+   
 
-`<header class="p-5 mb-4 header bg-light">
-<h2 class="card-title">${engeneer.getName()}</h2>
-<h2 class="card-title">${engeneer.getRole()}</h2>
+return `<header class="p-5 mb-4 header bg-light">
+<h2 class="card-title">${engineer.getName()}</h2>
+<h2 class="card-title">${engineer.getRole()}</h2>
 </header>
 
 <div class="card" style="width: 18rem;">
@@ -71,8 +39,8 @@ const htmlEngeneer = engeneer  => {
 
 </div>
 <ul class="list-group list-group-flush">
-  <li class="list-group-item">${engeneer.getId()}</li>
-  <li class="list-group-item">Email: <a href="mail:${engeneer.getEmail()}"></a></li>
+  <li class="list-group-item">${engineer.getId()}</li>
+  <li class="list-group-item">Email: <a href="mail:${engineer.getEmail()}"></a></li>
   <li class="list-group-item">GitHub: <a href="https://github.com/${engineer.getGithub()}" target="_blank" rel="noopener noreferrer"></a></li>
   
  
@@ -81,9 +49,9 @@ const htmlEngeneer = engeneer  => {
 `;
 
 };
-const htmlIntern = intern => {
-    return
-    `
+const generateIntern = intern => {
+    
+    return `
     <header class="p-5 mb-4 header bg-light">
     <h2 class="card-title">${intern.getName()}</h2>
     <h2 class="card-title">${intern.getRole()}</h2>
@@ -106,16 +74,16 @@ const htmlIntern = intern => {
 
 const html = [];
   
-html.push(profile
+html.push(team
     .filter(employee => employee.getRole() === "Manager")
     .map(manager => generateManager(manager))
 );
-html.push(profile
+html.push(team
     .filter(employee => employee.getRole() === "Engineer")
     .map(engineer => generateEngineer(engineer))
     .join("")
 );
-html.push(profile
+html.push(team
     .filter(employee => employee.getRole() === "Intern")
     .map(intern => generateIntern(intern))
     .join("")
@@ -124,5 +92,38 @@ html.push(profile
 return html.join("");
 
 
+}
+
+
+module.exports = team => {
+
+return `<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+<title>Document</title>
+</head>
+<body>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-12 jumbotron mb-3 team-heading bg-danger">
+            <h1 class="text-center text-white">My Team</h1>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="row">
+        <div class="row team-area col-12 d-flex justify-content-center">
+            ${generateProfile(team)}
+        </div>
+    </div>
+</div>
+
+
+</body>
+</html>
+`;
 }
   
